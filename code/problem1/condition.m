58a6b62f546376515322f074bdbf7c83ad25af23
Janus Troelsen <ysangkok@gmail.com>
2014-12-07 14:24:16 +0100

    conditioning tryouts; results from first session in c-pool

--- code/problem1/condition.m
+++ code/problem1/condition.m
@@ -9,5 +9,20 @@ function [U,T] = condition(points)
 %       T       3x3 conditioning matrix
 %%
 
-U =
-T =
+% Conditioning: scale and shift points to be in [-1..1]
+size(points)
+s = 0.5 * norm(points, 1)
+tx = mean(mean(points, 1))
+ty = mean(mean(points, 2))
+%size(t)
+T = [1/s 0 -tx/s;
+    0 1/s -ty/s;
+    0 0 1]
+U = imfilter(points, T, 'replicate');
+
+lastMean(U);
+assert(lastMean(1) == 0 && lastMean(2) == 0, ...
+    'Conditionned matrix should have mean of [0, 0].');
+assert(isfloat(U) & min(U(:)) >= 0 & max(U(:)) <= 1, ...
+    'The values of U should be floating point numbers between -1 and 1.');
+
--- code/problem2/problem2.m
+++ code/problem2/problem2.m
@@ -11,10 +11,27 @@ im2 = im2double(imread('a3p2b.png'));
 load('points.mat'); % gives you points1 and points2 variables
 
 % Display the correspondences:
+figure,
+imshow(im1)
+hold on 
+plot(points1(:,1),points1(:,2), 'yx')
+hold off
 
+figure,
+imshow(im2)
+hold on 
+plot(points2(:,1),points2(:,2) ,'yx')
+hold off
+
+%% Compute homogeneous coordinates
+siz = size(points1);
+a = ones([siz(1) 1]);
+hpoints1 = cat(2, points1, a)
+hpoints2 = cat(2, points2, a)
 
 % Compute the fundamental matrix using 'eightpoint':
-
+%% Compute the mean
+condition(hpoints1)
 
 % Draw epipolar lines using 'show_epipolar':
 
